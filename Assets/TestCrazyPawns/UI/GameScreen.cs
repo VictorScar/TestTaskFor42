80d9b7a40fe3c46b2c2db7b4f016f61ccfa3cddc
VictorScar <Babooner@mail.ru>
2025-08-12 21:20:24 +0400

    add; base ui logic

---
+++ Assets/TestCrazyPawns/UI/GameScreen.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ScarFramework.UI;
+using UnityEngine;
+
+public class GameScreen : UIScreen
+{
+    /*private Vector3 _lastPointerPos;
+    private float borderTreshhold;
+    private void Update()
+    {
+        var mousePos = Input.mousePosition;
+        var screenPoint = rect.InverseTransformPoint(mousePos);
+
+        CheskScreenBoreders(screenPoint);
+        
+        _lastPointerPos = screenPoint;
+    }
+
+    private void CheskScreenBoreders(Vector3 screenPoint)
+    {
+        Debug.Log("ScreenPoint = " + screenPoint);
+        Debug.Log("screen size = " + rect.sizeDelta.x);
+        
+        if (screenPoint.x - rect.sizeDelta.x <= borderTreshhold)
+        {
+            
+        }
+    }*/
+}
